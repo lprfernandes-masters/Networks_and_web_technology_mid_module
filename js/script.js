2da199878da1f81c0b1b087d492a004fdecec38f
Luis Fernandes <lprfernandes@gmail.com>
2025-07-14 09:11:33 +0200

    changed images and text details

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+/*document.addEventListener('DOMContentLoaded', function () {
     const toggleButton = document.querySelector('.navbar__mobile-menu-toggle');
     const mobileMenu = document.querySelector('.navbar__mobile-menu-items');
 
@@ -6,16 +6,5 @@ document.addEventListener('DOMContentLoaded', function () {
         mobileMenu.classList.toggle('active');
     });
 });
-
-window.addEventListener('scroll', function () {
-    const navbar = document.querySelector('.navbar');
-
-    if (window.scrollY > 0)
-    {
-        navbar.classList.add('navbar--scroll');
-    } else
-    {
-        
-        navbar.classList.remove('navbar--scroll');
-    }
-});
+*/
+   
